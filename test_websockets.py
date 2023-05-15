9d3f6aa684173cba54167eab43d1080cd31ef991
Rombout Jansen <romboutlambert@gmail.com>
2023-05-15 16:45:32 +0200

    Send rotation data to server

--- test_websockets.py
+++ test_websockets.py
@@ -1,15 +1,43 @@
 import asyncio
 from websockets.server import serve
+import time
+import board
+import adafruit_mma8451
+import math
 
 async def echo(websocket):
-    number = 0
-    await websocket.send(str(number))
+    x, y, z = sensor.acceleration
+    await websocket.send("0:0")
     async for message in websocket:
-        await websocket.send(str(number))
-        number += 1
+        x, y, z = sensor.acceleration
+        try:
+            pitch = math.degrees(math.atan(y / z))
+        except ZeroDivisionError:
+            pitch = 90 if y > 0 else -90
+
+        try:
+            roll = math.degrees(math.atan(x / z))
+        except ZeroDivisionError:
+            roll = 90 if x > 0 else -90
+
+        await websocket.send(f"{roll}:{pitch}")
 
 async def main():
-    async with serve(echo, "localhost", 8765):
+    async with serve(echo, "192.168.182.30", 8765):
+        print("Available 192.168.182.30:8765...")
         await asyncio.Future()  # run forever
+
+i2c = board.I2C()
+while True:
+    try:
+        print("Waiting for I2C...")
+        time.sleep(5)
+        # Initialize MMA8451 module.
+        sensor = adafruit_mma8451.MMA8451(i2c)
+        print("Done")
+        break
+    except OSError:
+        print("Connection failed...")
+        print("Trying again...")
 
 asyncio.run(main())
